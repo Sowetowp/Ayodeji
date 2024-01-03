9382cce640bc03bbda6a513da62a8d5bc0803bb3
Abdulmalik <ayodejiamzat@gmail.com>
2024-01-03 13:58:27 +0100

    work upload

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import menu from "../Assets/Images/menu.svg"
 import "../Styles/Menu.css"
 import close from "../Assets/Images/close.svg"
@@ -10,6 +10,15 @@ import "../Styles/Demo.css"
 
 
 const Main = () => {
+    const [route, setRoute] = useState("/home")
+    const routesetter = useCallback((e)=>{
+        setRoute(e)
+        localStorage.setItem('myAppState', JSON.stringify(e));
+    },[])
+    useEffect(()=>{
+        const savedState = JSON.parse(localStorage.getItem('myAppState'));
+        setRoute(savedState)
+    },[])
     const [counter, setCounter] = useState(0)
     const [image, setImage] = useState([
         menu, close
@@ -21,9 +30,10 @@ const Main = () => {
     const switc = () => {
         setChange(!change)
     }
-    const malik = () => {
+    const malik = (e) => {
         add()
         switc()
+        routesetter(e)
     }
 
 
@@ -33,12 +43,12 @@ const Main = () => {
             <div className='mmd'>
                 <div className='container-fluid sa'>
                     <div className='row text-center mrow p-0'>
-                        <a  href='/home' target="frame" onClick={malik} className='col-2 borde'><p className='menu'><i>0.1 </i>HOME</p></a>
-                        <a href='/about' target="frame" onClick={malik} className='col-2'><p className='menu'><i>0.2 </i>ABOUT</p></a>
-                        <a href='/resume' target="frame" onClick={malik} className='col-2 borde'><p className='menu'><i>0.3 </i>RESUME</p></a>
-                        <a href='/work' target="frame" onClick={malik} className='col-2'><p className='menu'><i>0.4 </i>WORKS</p></a>
-                        <a href='/blog' target="frame" onClick={malik} className='col-2 borde'><p className='menu'><i>0.5 </i>BLOGS</p></a>
-                        <a href='/contact' target="frame" onClick={malik} className='col-2'><p className='menu'><i>0.6 </i>CONTACT</p></a>
+                        <a  href='/home' target="frame" onClick={()=>malik("/home")} className='col-2 borde'><p className='menu'><i>0.1 </i>HOME</p></a>
+                        <a href='/about' target="frame" onClick={()=>malik("/about")} className='col-2'><p className='menu'><i>0.2 </i>ABOUT</p></a>
+                        <a href='/resume' target="frame" onClick={()=>malik("/resume")} className='col-2 borde'><p className='menu'><i>0.3 </i>RESUME</p></a>
+                        <a href='/work' target="frame" onClick={()=>malik("/work")} className='col-2'><p className='menu'><i>0.4 </i>WORKS</p></a>
+                        <a href='/blog' target="frame" onClick={()=>malik("/blog")} className='col-2 borde'><p className='menu'><i>0.5 </i>BLOGS</p></a>
+                        <a href='/contact' target="frame" onClick={()=>malik("/contact")} className='col-2'><p className='menu'><i>0.6 </i>CONTACT</p></a>
                     </div>
                 </div>
             </div>
@@ -58,7 +68,7 @@ const Main = () => {
             <div className='ifram'> 
                 <button className='mbut' onClick={switc}><img src={arro}/></button>
                 {change && <Menu/>}
-                <iframe src='/home' name='frame' title='myiframe' className='frame'></iframe>
+                <iframe src={route == null ? "/home" : route} name='frame' title='myiframe' className='frame'></iframe>
             </div>
         </div>
     </>
